fb6893d7394051308ae51da5570448db472a9cd3
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-09 14:43:17 -0800

    fin exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let result = "";
+  for (var i = string.length; i > -1; i--) {
+    result += string.charAt(i);
+  }
+  return result;
 };
 
 // Do not edit below this line
