879b5155a29ed8c0860c55b9db5b32aca29b015d
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-10 19:51:51 -0800

    fin exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  // loop from start to end integer, += to the sum
+  let sum = 0;
+  if (
+    start < 0 ||
+    end < 0 ||
+    Number.isInteger(start) == false ||
+    Number.isInteger(end) == false ||
+    typeof start != "number" ||
+    typeof end != "number"
+  ) {
+    return "ERROR";
+  } else if (start < end) {
+    for (var i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else {
+    // test 3 tests the case of start > end
+    for (var i = start; i >= end; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
