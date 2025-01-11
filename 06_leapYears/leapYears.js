7f9aece2dfb127a845618a53424958084e606f88
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-10 19:59:59 -0800

    fin exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // most common: div by 4
+  // least common: div by 400; check this before div 100
+  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
