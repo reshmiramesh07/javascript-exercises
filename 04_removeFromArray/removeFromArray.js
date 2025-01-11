28d6baacbfcbff9e8d4b9a4a085ea6d7ac917018
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-10 19:01:53 -0800

    fin exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeArgs) {
+  let result = array;
+  for (const arg of removeArgs) {
+    // if (arg in array == false) {
+    //   continue;
+    // }
+    while (result.indexOf(arg) != -1) {
+      removeAtIndex = result.indexOf(arg);
+      result.splice(removeAtIndex, 1);
+    }
+  }
+  return result;
+
+  // remove multiple of the same value:
+  // loop until that value isn't found in the array anymore?
 };
 
 // Do not edit below this line
