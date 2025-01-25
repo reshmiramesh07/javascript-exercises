5f716b42c7c2ca10bb65bc8671ec568a9911ea09
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-25 11:55:52 -0800

    fin exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const ageSort = people.sort(function (a, b) {
+    for (var i = 0; i < people.length; i++) {
+      if (people[i].yearOfDeath == undefined) {
+        people[i].yearOfDeath = new Date().getFullYear();
+      }
+    }
+    if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return ageSort[ageSort.length - 1];
 };
 
 // Do not edit below this line
