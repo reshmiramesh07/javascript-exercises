bc938a632e4d38c1c08e71ab0cc45a1e16af456f
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-25 10:50:36 -0800

    fin exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const strArr = string.toLowerCase().split("");
+  let forwards = "";
+  let backwards = "";
+  // remove spaces and any punctuation
+  for (var i = 0; i < strArr.length; i++) {
+    if (
+      strArr[i] == " " ||
+      strArr[i] == "," ||
+      strArr[i] == "." ||
+      strArr[i] == "!"
+    ) {
+      continue;
+    } else {
+      forwards += strArr[i];
+    }
+  }
+  console.log(forwards);
+  for (var i = forwards.length - 1; i > -1; i--) {
+    backwards += forwards[i];
+  }
+  console.log(backwards);
+  if (forwards == backwards) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
