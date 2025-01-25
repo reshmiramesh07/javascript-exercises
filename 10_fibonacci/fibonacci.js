ffed59d945161f9d3818bdf91b2af66782bc4dd8
Reshmi Ramesh <reshmiramesh07@gmail.com>
2025-01-25 11:27:33 -0800

    fin exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (nth) {
+  // "nth number returns x in the sequence"
+  // 1 1 2 3 5 8
+  // 1 2 3 4 5 6
+  // 0 1 2 3 4 5
+  let next = 1;
+  let current = 1;
+  let previous = 0;
+  if (nth == 0) {
+    return previous;
+  } else if (Number(nth) < 0) {
+    return "OOPS";
+  }
+  for (var i = 1; i < Number(nth); i++) {
+    current = next;
+    next = current + previous;
+    previous = current;
+  }
+  return next;
 };
 
 // Do not edit below this line
